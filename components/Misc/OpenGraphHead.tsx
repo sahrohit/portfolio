7d1bd6324350130bd6fdeeea0943a6a88b1857f2
sahrohit <sahrohit9586@hmail.com>
2021-10-10 12:38:01 +0545

    Added Meta Tags

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -1,19 +1,29 @@
 import Head from 'next/head'
 const OpenGraphHead = () => (
   <Head>
-    <title>KL Lawingco | Software Engineer</title>
-    <meta name="description" content="My personal solace place on web-earth." />
-    <meta property="og:title" content="KL Lawingco | Software Engineer" />
-    <meta property="og:site_name" content="Marc Jhon Lawingco" />
-    <meta property="og:url" content="https://kllawingco.netlify.app/" />
+    <title>Rohit Sah | Junior Web Developer</title>
+    <meta name="title" content="Rohit Sah | Junior Web Developer" />
+    <meta name="description" content="The only portfolio I like." />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.sahrohit.com.np/" />
+    <meta property="og:title" content="Rohit Sah | Junior Web Developer" />
     <meta
       property="og:description"
-      content="Ohh you found me?. Howdy! I am Marc Jhon Lawingco. Or you could call me KL. That works too . . . I am a Software Engineer"
+      content="I am Sah Rohit, officially Rohit Kumar Sah . . . I am a Computer Engineer"
     />
-    <meta property="og:type" content="profile" />
+    <meta property="og:image" content="https://sahrohit.com.np/avatar.png" />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://www.sahrohit.com.np/" />
+    <meta property="twitter:title" content="Rohit Sah | Junior Web Developer" />
     <meta
-      property="og:image"
-      content="https://kllawingco.netlify.app/KL_avatar.png"
+      property="twitter:description"
+      content="I am Sah Rohit, officially Rohit Kumar Sah . . . I am a Computer Engineer"
+    />
+    <meta
+      property="twitter:image"
+      content="https://sahrohit.com.np/avatar.png"
     ></meta>
   </Head>
 )
